e0b848f12fd06aea08ab4fbf99ff41079d9c5d44
Bong Yeon, Kim <kimbongyeon83@gmail.com>
2016-11-08 13:51:23 +0000

    project initializing..

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the SplitSens package.
+ *
+ * (c) Bong Yeon, Kim <kimbongyeon83@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
